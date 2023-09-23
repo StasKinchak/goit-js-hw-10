34b9afcf4c0299ddc989966360c662985fa10e18
StasKinchak <130861052+StasKinchak@users.noreply.github.com>
2023-09-23 16:27:18 +0200

    work

---
+++ src/cat-api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] = 'llive_FFvfduuK7VX8L0fcMGeXqcOTqqUfq741DmMNvhDWFUEryQpwxGicHwgLBJ6D6J8C';
+
+
+export async function fetchBreeds() {
+  try {
+    const response = await axios.get('https://api.thecatapi.com/v1/breeds');
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+export async function fetchCatByBreed(breedId) {
+  try {
+    const response = await axios.get(
+      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
